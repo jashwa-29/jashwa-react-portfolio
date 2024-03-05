8e38950ae39291d7a3c39fe49d3f092175d2583a
jashwa-29 <153584496+jashwa-29@users.noreply.github.com>
2024-03-05 21:48:41 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
   };
  
   return (
-    <div className='contact'>
+      <div className='contact'>
       <div className='container'>
       <div className="contact-heading">
             <h2>GET IN TOUCH</h2>
             <p>If you like my Projects and Portfolio, kindly contact me here</p>
         </div>
       <div className="main-con-flex">
-             <div className="form-box" data-aos="fade-down"
+             <div className="form-box" data-aos="zoom-in-up"
      data-aos-offset="310"
-     data-aos-easing="ease-in-sine">
+     data-aos-easing="ease-in-sine" data-aos-delay="100">
                  <form ref={form} onSubmit={sendEmail}>
                 
                     <div className="input-flex">
@@ -53,13 +53,13 @@ const Contact = () => {
 
                 </form> 
              </div>
-             <div className='also-con-box' data-aos="fade-up"      data-aos-offset="310"
-     data-aos-easing="ease-in-sine">
-                    <h4>Also Contact me in </h4>
-                    <p><span class="material-symbols-outlined">
+             <div className='also-con-box  contact-info' data-aos="zoom-in-down"      data-aos-offset="310"
+     data-aos-easing="ease-in-sine" data-aos-delay="100">
+                    <h4 className='content'>Also Contact me in </h4>
+                    <p className='content'><span class="material-symbols-outlined">
 mail
 </span>jashwa46733@gmail.com</p>
-                    <p><i class="bi bi-telephone-fill"></i>+91 7092854846</p>
+                    <p className='content'><i class="bi bi-telephone-fill"></i>+91 7092854846</p>
              </div>
       </div>
       </div>
